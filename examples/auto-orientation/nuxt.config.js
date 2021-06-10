a8ccd6077bc1298b3b733b846e0be539bbefe3cd
Ivo Dolenc <ivodolenc@gmail.com>
2021-06-10 18:58:12 +0200

    docs: add new 'autoOrientation' example

---
+++ examples/auto-orientation/nuxt.config.js
@@ -0,0 +1,9 @@
+export default {
+  css: ['~/assets/css/main.css'],
+
+  buildModules: ['nuxt-bowser'],
+
+  bowser: {
+    autoOrientation: true
+  }
+}
---
+++ examples/auto-orientation/pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <section>
+    <div v-if="$browser.is('mobile')">
+      <h3>Mobile</h3>
+    </div>
+    <div v-else-if="$browser.is('tablet')">
+      <h3>Tablet</h3>
+    </div>
+    <div v-else>
+      <h3>Desktop</h3>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {}
+</script>
